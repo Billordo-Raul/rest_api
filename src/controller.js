b9550bffbcbc509ed38bee791a7965fabf815eab
[billordo-raul] <billordo786@gmail.com>
2023-10-19 10:44:33 -0300

    Alta de Registros

--- src/controller.js
+++ src/controller.js
@@ -13,6 +13,24 @@ class LibroController{
 		res.json(result);
 	}
 
+	async agregar (req,res){
+	    const libro = req.body;
+	    try{
+		/* Agregar registro */
+		const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,anio_publicacion,ISBN) VALUES (?,?,?,?,?)`,[libro.nombre,libro.autor,libro.categoria,libro.anio_publicacion,libro.ISBN]);
+		res.json({"ID insertado":result.insertId});
+		res.json(result);
+		 
+	    }catch (e){
+
+	        console.log( e);
+		const Error = e.message;
+		res.status(400).json({Error });	
+	    }
+
+	}
+
 }
+
 
 export const libro = new LibroController();
--- src/routes.js
+++ src/routes.js
@@ -5,3 +5,5 @@ export const router = Router()
 
 router.get('/libros',libro.getAll);
 router.get('/consulta',libro.getOne);
+
+router.post('/agregar',libro.agregar); 
